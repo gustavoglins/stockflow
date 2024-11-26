ae5c06b23b5dbfefff095548fa072a2da8ee8cd8
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-11-26 12:58:14 -0300

    feat: finish Home page

--- src/main/java/com/stockflow/controllers/PagesController.java
+++ src/main/java/com/stockflow/controllers/PagesController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class PagesController {
 
     @GetMapping("/home")
     public String homePage(Model model) {
