4dc4369872008643f26d0a12ab095ee705c7236c
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-12-01 19:41:13 -0300

    refactor: removing unuseds instances

--- src/main/java/com/stockflow/controllers/AuthenticationController.java
+++ src/main/java/com/stockflow/controllers/AuthenticationController.java
@@ -1,34 +1,28 @@
 package com.stockflow.controllers;
 
-import com.stockflow.dto.user.UserLoginRequestDTO;
-import com.stockflow.dto.user.UserLoginResponseDTO;
-import com.stockflow.dto.user.UserSignupRequestDTO;
-import com.stockflow.dto.user.UserSignupResponseDTO;
+import com.stockflow.dto.user.login.UserLoginRequestDTO;
+import com.stockflow.dto.user.login.UserLoginResponseDTO;
 import com.stockflow.infra.security.TokenService;
 import com.stockflow.model.user.User;
-import com.stockflow.repositories.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
     private final TokenService tokenService;
-    private PasswordEncoder passwordEncoder;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository, TokenService tokenService, PasswordEncoder passwordEncoder) {
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
         this.tokenService = tokenService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
