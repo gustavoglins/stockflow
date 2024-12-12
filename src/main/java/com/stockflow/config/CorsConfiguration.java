d7fa9fa92dd5bc9b8fd79a03d6257c994577288d
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-12-12 03:48:34 -0300

    feat: add Cors config

---
+++ src/main/java/com/stockflow/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.stockflow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:8080", "http://localhost:4200")
+                .allowedOrigins("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
