77255f0f34fbd8d6a6e6ab908d20f052878e14bd
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-12-12 15:37:01 -0300

    feat: create User basic managements

---
+++ src/main/java/com/stockflow/api/controllers/UserController.java
@@ -0,0 +1,43 @@
+package com.stockflow.api.controllers;
+
+import com.stockflow.api.requests.UserUpdateRequestDTO;
+import com.stockflow.api.responses.UserDetailsResponseDTO;
+import com.stockflow.domain.services.interfaces.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDetailsResponseDTO> update(@RequestBody @Valid UserUpdateRequestDTO userUpdateRequestDTO) {
+        return ResponseEntity.ok(userService.update(userUpdateRequestDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDetailsResponseDTO> get(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDetailsResponseDTO>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
