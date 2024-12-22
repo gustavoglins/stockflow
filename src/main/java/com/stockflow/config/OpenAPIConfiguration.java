04f0a6c513edf5309c6668c2fd1a594e523f93c8
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-12-22 14:41:27 -0300

    feat: create OpenAPI config class

---
+++ src/main/java/com/stockflow/config/OpenAPIConfiguration.java
@@ -0,0 +1,37 @@
+package com.stockflow.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(
+        title = "StockFlow API",
+        version = "1.0.0",
+        description = "Documentation for the StockFlow API, an inventory management platform with features for managing products in real time. The API provides endpoints for user and product management.",
+        termsOfService = "https://github.com/gustavoglins/stockflow/blob/master/TERMS.md",
+        contact = @Contact(name = "Suporte Técnico", email = "gustavoglins05@gmail.com", url = "mailto:gustavoglins05@gmal.com"),
+        license = @License(name = "MIT License", url = "https://github.com/gustavoglins/stockflow/blob/master/LICENSE.md")
+))
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
+    }
+}
